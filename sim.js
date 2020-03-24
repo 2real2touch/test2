b2def655b3dd92cb3243c1ad2448d62e6d800ba3
brainlessYo <62247863+brainlessYo@users.noreply.github.com>
2020-03-24 11:54:48 +0200

    Add files via upload

---
+++ sim.js
@@ -0,0 +1,89 @@
+var myData = [{"ID":3,"Name":"alphasms.ua","Ico":"https://ilogik.planfix.ru/img/sms/alphasms.png","Params":[{"Name":"API Key","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"Для того, чтобы найти API Key, необходимо зайти: Мой офис – настройки – API.","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":25,"Name":"aramba.ru","Ico":"https://ilogik.planfix.ru/img/sms/aramba.png","Params":[{"Name":"API-ключ","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":21,"Name":"atompark.com","Ico":"https://ilogik.planfix.ru/img/sms/atompark.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"Для активации смс шлюза необходимо в панели управления сервисом в «Настройках» активировать использование API.<br>Для этого в пункте «XML интерфейс включен» необходимо выбрать пункт «Да». <br>В пункте «XML интерфейс: режим работы» выбрать либо «Реальная отправка», либо «Тестовый режим».","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":6,"Name":"bulksms.com","Ico":"https://ilogik.planfix.ru/img/sms/bulksmscom.png","Params":[{"Name":"Username","Type":0,"Acr":"login"},{"Name":"Password","Type":0,"Acr":"password"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":24,"Name":"devinotele.com","Ico":"https://ilogik.planfix.ru/img/sms/devinotelecom.svg","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":30,"Name":"easysendsms.com","Ico":"https://ilogik.planfix.ru/img/sms/easysendsmscom.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":12,"Name":"esputnik.com",	"Ico":"https://ilogik.planfix.ru/img/sms/esputnikcom.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":23,"Name":"Zadarma","Ico":"https://ilogik.planfix.ru/img/tel/zadarma_big.png","Params":[{"Name":"API Key","Type":0,"Acr":"apiKey"},{"Name":"Secret Key","Type":0,"Acr":"secretKey"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":20,"Name":"zazumedia.ru","Ico":"https://ilogik.planfix.ru/img/sms/zazumedia.png","Params":[{"Name":"ID","Type":0,"Acr":"login"},{"Name":"API ключ","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":11,"Name":"infobip.com","Ico":"https://ilogik.planfix.ru/img/sms/infobipcom.svg","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":5,"Name":"iqsms.ru","Ico":"https://ilogik.planfix.ru/img/sms/iqsms.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":36,"Name":"<a href=\"https://justsend.pl\" target=\"_blank\">justsend.pl</a>","Ico":"https://ilogik.planfix.ru/img/sms/justsendpl.svg","Params":[{"Name":"Klucz Api","Type":0,"Acr":"apiKey"},{"Name":"Nadawca","Type":0,"Acr":"from"},{"Name":"Wariant wysułki","Type":0,"Acr":"bulkVariant"}],"ParamsText":"Wariant wysułki : dostępne wartośc<ul>\n     <li>              -- ECO - Basic</li>\n     <li>              -- FULL - UNIQUE</li>\n     <li>              -- PRO - DYNAMIC</li>\n     <li>              -- VOICE - VOICE</li>\n     <li>              -- ECO_RESP - Response z numeru 4777</li>\n     <li>              -- PRO_RESP - Response z Nadpisem.</li></ul>","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":22,"Name":"МегаФон","Ico":"https://ilogik.planfix.ru/img/tel/megafon_big.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"IP-адреса ПланФикса, с которых будет происходить работа с API:<br>144.76.68.177<br>144.76.68.178<br>144.76.68.179<br>144.76.68.180<br>144.76.68.181<br>144.76.111.15<br>88.99.219.48<br>","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":19,"Name":"mobizon.ua","Ico":"https://ilogik.planfix.ru/img/sms/mobizon.svg","Params":[{"Name":"API Key","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":32,"Name":"<a href=\"https://admin.p1sms.ru/ru-ru/registration.html?utm_source=planfix\" target=\"_blank\" rel=\"noopener\">p1sms.ru</a>","Ico":"https://ilogik.planfix.ru/img/sms/p1smsru.png","Params":[{"Name":"API-ключ","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":16,"Name":"prostor-sms.ru","Ico":"https://ilogik.planfix.ru/img/sms/prostorsms.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":26,"Name":"redsms.ru","Ico":"https://ilogik.planfix.ru/img/sms/redsms.jpg","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"API-ключ","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"Для активации работы с API Вам необходимо в личном кабинете<br>в <a href=\"https://cp.redsms.ru/settings/\">настройках пользователя</a> заполнить api-key.","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":29,"Name":"rocketsms.by","Ico":"https://ilogik.planfix.ru/img/sms/rocketsmsby.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":14,"Name":"qtelecom.ru","Ico":"https://ilogik.planfix.ru/img/sms/qtelecom.gif","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":27,"Name":"sigmasms.ru","Ico":"https://ilogik.planfix.ru/img/sms/sigmasms.png","Params":[{"Name":"Токен","Type":0,"Acr":"token"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"Для работы с API Вам необходимо запросить специальный токен в поддержке sigmasms.ru","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":34,"Name":"slt-telecom.com","Ico":"https://ilogik.planfix.ru/img/sms/slttelecom.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":1,"Name":"sms.ru","Ico":"https://ilogik.planfix.ru/img/sms/smsru.png","Params":[{"Name":"API ID","Type":0,"Acr":"apiId"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":38,"Name":"<a href=\"https://sms4b.ru\" target=\"_blank\">sms4b.ru</a>","Ico":"https://ilogik.planfix.ru/img/sms/sms4bru.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+
+{"ID":40,"Name":"<a href=\"https://smsaero.ru\" target=\"_blank\">smsaero.ru</a>","Ico":"https://ilogik.planfix.ru/img/sms/smsaeroru.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"API ключ","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"},{"Name":"Канал отправки","Type":0,"Acr":"channel"}],"ParamsText":"Каналы отправки<ul>     <li>              INFO - Инфоподпись для всех операторов</li>     <li>              DIGITAL - Цифровой канал отправки (допускается только транзакционный трафик)</li>     <li>              INTERNATIONAL - Международная доставка (Операторы РФ, Казахстана, Украины и Белоруссии)</li>     <li>              DIRECT - Рекламный канал отправки сообщений с бесплатной буквенной подписью</li>     <li>              SERVICE - Сервисный канал для отправки сервисных SMS по утвержденному шаблону с платной подписью отправителя</li></ul>","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+
+{"ID":37,"Name":"<a href=\"https://www.smsapi.pl\" target=\"_blank\">smsapi.pl</a>","Ico":"https://ilogik.planfix.ru/img/sms/smsapipl.png","Params":[{"Name":"Token API","Type":0,"Acr":"apiKey"},{"Name":"Nadawca","Type":0,"Acr":"from"}],"ParamsText":"Generowanie tokenu z dostępem do wybranych stref można zrealizować w panelu klienta <a href=\"https://ssl.smsapi.pl/react/oauth/manage\" target=\"_blank\">Tokeny API</a>","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":9,"Name":"sms-assistent.by","Ico":"https://ilogik.planfix.ru//img/sms/smsassistentby.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль для API","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":2,"Name":"smsbliss.ru","Ico":"https://ilogik.planfix.ru//img/sms/smsbliss.gif","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":39,"Name":"<a href=\"https://www.smsvizitka.com\" target=\"_blank\">smsvizitka.com</a>","Ico":"https://ilogik.planfix.ru/img/sms/smsvizitkacom.png","Params":[{"Name":"Ключ API","Type":0,"Acr":"api_key"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":33,"Name":"smsgorod.ru","Ico":"https://ilogik.planfix.ru/img/sms/smsgorod.jpg","Params":[{"Name":"API-ключ","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":18,"Name":"smsint.ru","Ico":"https://ilogik.planfix.ru/img/sms/smsintru.jpg","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":28,"Name":"smsclub.mobi","Ico":"https://ilogik.planfix.ru/img/sms/smsclubmobi.jpg","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Токен","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":10,"Name":"smsp.by","Ico":"https://ilogik.planfix.ru/img/sms/smspby.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Ключ API","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":7,"Name":"smspilot.ru","Ico":"https://ilogik.planfix.ru/img/sms/smspilot.png","Params":[{"Name":"API Key","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"API Key: <a href=\"https://www.smspilot.ru/my-settings.php#api\" target=\"_blank\">https://www.smspilot.ru/my-settings.php#api</a><br>Имя отправителя: <a href=\"https://www.smspilot.ru/my-sender.php\" target=\"_blank\">https://www.smspilot.ru/my-sender.php</a>","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":15,"Name":"smsprofi.ru","Ico":"https://ilogik.planfix.ru/img/sms/smsprofiru.jpg","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":35,"Name":"smsimple.ru","Ico":"https://ilogik.planfix.ru/img/sms/smsimpleru.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Номер подписи","Type":0,"Acr":"or_id"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":31,"Name":"sms-uslugi.ru","Ico":"https://ilogik.planfix.ru/img/sms/smsuslugiru.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":13,"Name":"sms-fly.ua","Ico":"https://ilogik.planfix.ru/img/sms/smsflyua.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":17,"Name":"smsc.ru","Ico":"https://ilogik.planfix.ru/img/sms/smscru.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":4,"Name":"Tele2","Ico":"https://ilogik.planfix.ru/img/sms/tele2.png","Params":[{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"Услуга <a href=\"https://msk.tele2.ru/business/option/business-sms\" target=\"_blank\">Бизнес-SMS</a>","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":8,"Name":"unisender","Ico":"https://ilogik.planfix.ru/img/sms/unisender.svg","Params":[{"Name":"API Key","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":41,"Name":"<a href=\"https://webcom.mobi\" target=\"_blank\">webcom.mobi</a>","Ico":"https://ilogik.planfix.ru/img/sms/webcommobi.jpg","Params":[{"Name":"API Key","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя SMS","Type":0,"Acr":"from"}],"ParamsText":"API Key: <a href=\"http://my2.webcom.mobi/#/api\">http://my2.webcom.mobi/#/api</a><br>\n\t\t\t\t\tИмя отправителя: <a href=\"http://my2.webcom.mobi/#/components\">http://my2.webcom.mobi/#/components</a>","HelpLink":"https://planfix.ru/docs/Интеграция_с_SMS","Data":[]},
+{"ID":42,"Name":"API ПланФикса<br>(по логину и паролю)","Ico":"https://ilogik.planfix.ru/img/sms/api.svg","Params":[{"Name":"Адрес сервиса отправки","Type":0,"Acr":"url"},{"Name":"Логин","Type":0,"Acr":"login"},{"Name":"Пароль","Type":0,"Acr":"password"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/API_для_отправки_sms","Data":[]},
+{"ID":43,"Name":"API ПланФикса<br>(по ключу доступа)","Ico":"https://ilogik.planfix.ru/img/sms/api.svg","Params":[{"Name":"Адрес сервиса отправки","Type":0,"Acr":"url"},{"Name":"API Key","Type":0,"Acr":"apiKey"},{"Name":"Имя отправителя","Type":0,"Acr":"from"}],"ParamsText":"","HelpLink":"https://planfix.ru/docs/API_для_отправки_sms","Data":[]}];
+
+	 $.each(myData,function(index,item){
+
+      var brick=''+'<div class="container">'
+		+'<p>{{{Name}}}</p>'
+		+'<div><img src="{{Ico}}" class="pic"></div>'
+		+'<div class="cnt" >'
+		+	'<input type= "checkbox">'
+		+'</div>'
+		+'</div>';
+
+			const	tes=$(Mustache.render(brick,item));
+			 tes.click(function () {
+			 	
+			    	var wind = ''
+			      +'<div class="modal"></div>'
+					+'<div class="modal-wrapper">'
+					+	'<div class="modal-body">'
+					+		'<div id="modtext">{{{ParamsText}}}</div>'
+
+					+	'{{#Params}} <div class="rows"><label>{{Name}}</label>\n <input type="text" class="field"></div>\n{{/Params}}'
+					+	'</div>'
+					+ 	'<div class="modal-footer">'
+					+		'<button class="closeBtn" id="cancel">Отмена</button>'
+					+		'<button class="closeBtn" id="save">Сохранить</button>'
+					+	'</div>'
+					+'</div>'
+				const idc=$(Mustache.render(wind,item));
+				$('body').append(idc);
+
+				idc.find('.modal').css('display','block');
+				idc.find('.modal-wrapper').css('display','block');
+				tes.find('.cnt input:checkbox').prop('checked', true);
+				idc.find('.closeBtn').click(function (){
+					idc.css('display','none');
+					tes.find('.cnt input:checkbox').prop('checked', false);
+				})
+				$('.modal').click(function (){
+					idc.css('display','none');
+					tes.find('.cnt input:checkbox').prop('checked', false);
+				})
+			})
+        $('body').append(tes);
+    });
